047394f22a0150dc31d6a5b7cf557add8e538a45
hakase1983 <son.of.liberty1983@gmail.com>
2019-08-11 17:42:47 +0900

    非同期通信　追加

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,51 @@
+$(function(){
+
+  function new_message(message){
+    var html=`<div class="message">
+              <div class="upper-message">
+              <div class="upper_info_user">
+              ${message.name}
+              <span class="upper_info_data">
+              ${message.created_at}
+              </span>
+              </div>
+              </div>
+              <div class="lower-message">
+              <p class="lower-message__content">
+              ${message.content}
+              </p>
+              </div>
+              </div>`
+
+    return html;
+  }
+  
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action'); 
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function(message){
+        console.log(message)
+       var html = new_message(message);
+       $('.messages').append(html);
+       $('.form__message').val("");
+       $('.form__submit').prop('disabled',false);
+       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+       $('.form__submit').attr('disabled',false);
+      })
+
+      .fail(function(message){
+        console.log('error');
+
+      });
+    
+    })
+})

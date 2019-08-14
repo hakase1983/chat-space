2380f4d0cdb214ff94fcfdff27b95367aeebb2e5
hakase1983 <son.of.liberty1983@gmail.com>
2019-08-14 21:08:37 +0900

    自動更新機能追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,18 @@
-$(function(){
+$(document).on('turbolinks:load',function(){
   function new_message(message){
     var img = message.image.url ? `<img src= ${ message.image.url }>` : "";
     var html= `<div class="message">
-                <div class="upper-message">
-                  <div class="upper_info_user">
-                    ${message.name}
-                      <span class="upper_info_data">
+                <div data-id=${message.id}>
+                  <div class="upper-message">
+                    <div class="upper_info_user">
+                      ${message.name}
+                        <span class="upper_info_data">
                         ${message.created_at}
                       </span>
+                    </div>                
                   </div>
                 </div>
-               <div class="lower-message">
+              <div class="lower-message">
                   <p class="lower-message__content">
                     ${message.content}
                     ${img}
@@ -19,8 +21,7 @@ $(function(){
               </div>`
 
     return html;
-  }
-  
+  }  
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -35,14 +36,39 @@ $(function(){
     })
       .done(function(message){
         console.log(message)
-       var html = new_message(message);
-       $('.messages').append(html);
-       $('form')[0].reset();
-       $('.form__submit').prop('disabled',false);
-       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      var html = new_message(message);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.form__submit').prop('disabled',false);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},'fast');
       })
       .fail(function(message){
         alert('error');
         });
-    　})
-　})
+      })
+      
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("id");
+        $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {last_id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML='';
+        messages.forEach(function (message) {
+        insertHTML = new_message(message);
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        // $("どこを？").animate({なにを？},"fast")
+      });
+      })
+      .fail(function(){
+        console.log(this);
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+});
